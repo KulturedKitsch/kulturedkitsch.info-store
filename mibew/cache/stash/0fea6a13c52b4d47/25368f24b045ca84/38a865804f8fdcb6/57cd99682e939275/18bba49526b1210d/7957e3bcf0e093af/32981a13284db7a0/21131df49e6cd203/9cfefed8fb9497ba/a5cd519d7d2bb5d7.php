de9eaa28b74445fcc46054809c3277901a5c109d
Jeremy Evans <jeremy@kulturedkitsch.com>
2015-10-07 02:31:22 -0400

    mibew installed, style tweaks

---
+++ mibew/cache/stash/0fea6a13c52b4d47/25368f24b045ca84/38a865804f8fdcb6/57cd99682e939275/18bba49526b1210d/7957e3bcf0e093af/32981a13284db7a0/21131df49e6cd203/9cfefed8fb9497ba/a5cd519d7d2bb5d7.php
@@ -0,0 +1,17 @@
+<?php 
+/* Cachekey: cache/stash_default/translation/js/en/ */
+/* Type: array */
+/* Expiration: 2015-10-11T10:04:17-04:00 */
+
+
+
+$loaded = true;
+$expiration = 1444572257;
+
+$data = array();
+
+/* Child Type: string */
+$data['return'] = "Mibew.Localization.set([]);";
+
+/* Child Type: integer */
+$data['createdOn'] = 1444194515;
